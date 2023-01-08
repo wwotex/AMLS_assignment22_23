c0d2c546bfe9e22ca43984cd720f05eed1e059b3
wwotex <wwojtek55@gmail.com>
2023-01-08 23:16:16 +0000

    added all solutions to A1

--- A1/feature_extraction.py
+++ A1/feature_extraction.py
@@ -4,9 +4,9 @@ from keras_preprocessing import image
 import cv2
 import dlib
 
-# how much data to use
-training_N = 5000
-test_N = 1000
+# # how much data to use
+# training_N = 200
+# test_N = 20
 
 # define paths
 base_dir = os.path.dirname(__file__)
@@ -84,7 +84,7 @@ def run_dlib_shape(image):
 
     return dlibout, resized_image
 
-def extract_features_labels(training):
+def extract_features_labels(training, training_N, test_N):
     images_dir = training_images_dir if training else test_images_dir
     dataset_dir = training_set_dir if training else test_set_dir
     N = training_N if training else test_N
@@ -118,3 +118,37 @@ def extract_features_labels(training):
     all_labels = (np.array(all_labels) + 1)/2 
     return landmark_features, all_labels
 
+
+
+def get_images_labels(training, training_N, test_N):
+    images_dir = training_images_dir if training else test_images_dir
+    dataset_dir = training_set_dir if training else test_set_dir
+    N = training_N if training else test_N
+    # array of image paths
+    image_paths = [os.path.join(images_dir, l) for l in os.listdir(images_dir)[:N]]
+    
+    # load labels.csv
+    labels_file = open(os.path.join(dataset_dir, labels_filename), 'r')
+    lines = labels_file.readlines()
+
+    # make list of gender labels
+    gender_labels = {line.split('\t')[0] : int(line.split('\t')[2]) for line in lines[1:]}
+    all_features = []
+    all_labels = []
+    
+    # get features from each image with dlib
+    for img_path in image_paths:
+        file_name= img_path.split('.')[-2].split('\\')[-1]
+        # load image
+        img = image.img_to_array(
+            image.load_img(img_path,
+                            target_size = None,
+                            interpolation = 'bicubic'))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        all_features.append(img)
+        all_labels.append(gender_labels[file_name])
+
+    landmark_features = np.array(all_features)
+    # convert (-1,1) into (0,1)
+    all_labels = (np.array(all_labels) + 1)/2 
+    return landmark_features, all_labels
--- A1/model_selection.py
+++ A1/model_selection.py
@@ -1,17 +1,48 @@
-from os.path import join
+import os
 import feature_extraction as fe
 import numpy as np
 from sklearn.metrics import classification_report,accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn import svm
 import time
+from datetime import datetime
+import warnings
+
+# -----------------------------CONFIG-------------------------------
+warnings.filterwarnings('ignore')
+useSavedFeatures = True
+
+base_dir = os.path.dirname(__file__)
+global full_output
+full_output = ''
+
+def printWW(str):
+    global full_output
+    print(str)
+    full_output += str
+
+
+def saveOutputToFile():
+    global full_output
+    full_output = f'Datetime: {datetime.now()}\n\n train_N: {train_N}\t\ttest_N: {test_N}\n\n' + full_output + '\n\n\n\n'
+    with open(os.path.join(base_dir, 'model_selection_output.txt'), 'a') as f:
+        f.write(full_output)
+
+    pass
 
 
 def get_data():
-    
-    tr_X, tr_y = fe.extract_features_labels(training=True)
-    te_X, te_y = fe.extract_features_labels(training=False)
-    
+    path = os.path.join(base_dir, 'saved_features.npy')
+
+    if useSavedFeatures and os.path.exists(path):
+        tr_X, tr_y, te_X, te_y = np.load(path, allow_pickle=True)
+    else:
+        tr_X, tr_y = fe.extract_features_labels(training=True)
+        te_X, te_y = fe.extract_features_labels(training=False)
+        
+        np.save(path, [tr_X, tr_y, te_X, te_y])
+
     # tr_X, te_X, tr_Y, te_Y = train_test_split(X, y, test_size=0.10, random_state=42)
 
     train_N = tr_X.shape[0]
@@ -20,15 +51,32 @@ def get_data():
     return train_N, test_N, tr_X, tr_y, te_X, te_y
 
 def img_SVM(training_images, training_labels, test_images, test_labels):
-    classifier = svm.SVC(kernel='linear')
 
-    classifier.fit(training_images, training_labels)
+    # defining parameter range 
+    param_grid = {'C': [0.1, 1, 10, 10e2, 10e3, 10e4, 10e5],
+                'gamma': [10e-5, 10e-4, 10e-3, 10e-2, 10e-1, 1, 10],
+                'kernel': ['rbf']
+                }  
+    
+    grid = GridSearchCV(svm.SVC(), param_grid, refit = True, verbose = 3, n_jobs=-1) 
 
-    pred = classifier.predict(test_images)
+    printWW(f'\n\nparam grid used: \n{param_grid}\n\n')
 
-    print(pred)
+    # fitting the model for grid search 
+    grid.fit(training_images, training_labels) 
+    
+    # printWW best parameter after tuning 
+    printWW(f'best params: {grid.best_params_}\n\n')
+    printWW(f'best estimator: {grid.best_estimator_}\n\n') 
 
-    print("Accuracy:", accuracy_score(test_labels, pred))
+    
+    pred = grid.predict(test_images)
+
+    printWW(f'Prediction: {pred}\n\n')
+
+    printWW(f'Accuracy: {accuracy_score(test_labels, pred)}\n\n')
+
+    printWW(f'Classification report: {classification_report(test_labels, pred)}\n\n')
 
 
 start_time = time.time()
@@ -36,4 +84,6 @@ start_time = time.time()
 train_N, test_N, tr_X, tr_y, te_X, te_y= get_data()
 pred=img_SVM(tr_X.reshape((train_N, 68*2)), tr_y, te_X.reshape((test_N, 68*2)), te_y)
 
-print("\n\n--- %s seconds ---\n\n" % (time.time() - start_time))
+printWW("\n\n--- %s seconds ---\n\n" % (time.time() - start_time))
+
+saveOutputToFile()
